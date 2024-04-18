db26a7a0c4f55e64583c123c2f9dcc17a6791704
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-04-18 22:44:11 +0200

    5151main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ status = os.getenv("status")  # online/dnd/idle
 
 custom_activity = {
     "name": "nothing",
-    "type": 2,  # 2 for listening
+    "type": 2,  # 2 for listenig
 }
 
 usertoken = os.getenv("token")
